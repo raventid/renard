63ffc8e4688fbd253f9e0173b702108e26ff3b96
Julian Pokrovsky <juliankul@gmail.com>
2019-03-03 17:03:55 +0300

    extending lexer with more keywords, handle identifiers correctly

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,7 @@
 use crate::token;
-use std::str;
 
-#[derive(Default, Debug)]
+// TODO: Why byte? Maybe just use char and support unicode language out of box? wtp?
+#[derive(Default, Debug, Clone)]
 struct Lexer {
     input: String,
     position: u32,
@@ -40,9 +40,63 @@ impl Lexer {
 
     // TODO: rethink types. Current one are not very nice.
     fn next_token(&mut self) -> token::Token {
+        self.skip_whitespace();
+
         let tok = match self.ch {
-            b'=' => token::Token {
-                token_type: token::ASSIGN.to_string(),
+            b'=' => {
+                if self.peek_char() == b'=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    let literal = [ch, self.ch].iter().map(|&c| c as char).collect::<String>();
+                    token::Token {
+                        token_type: token::EQ.to_string(),
+                        literal,
+                    }
+                } else {
+                    token::Token {
+                        token_type: token::ASSIGN.to_string(),
+                        literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+                    }
+                }
+            }
+            b'+' => token::Token {
+                token_type: token::PLUS.to_string(),
+                literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+            },
+            b'-' => token::Token {
+                token_type: token::MINUS.to_string(),
+                literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+            },
+            b'!' => {
+                if self.peek_char() == b'=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    let literal = [ch, self.ch].iter().map(|&c| c as char).collect::<String>();
+                    token::Token {
+                        token_type: token::NOT_EQ.to_string(),
+                        literal,
+                    }
+                } else {
+                    token::Token {
+                        token_type: token::BANG.to_string(),
+                        literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+                    }
+                }
+            }
+            b'/' => token::Token {
+                token_type: token::SLASH.to_string(),
+                literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+            },
+            b'*' => token::Token {
+                token_type: token::ASTERISK.to_string(),
+                literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+            },
+            b'<' => token::Token {
+                token_type: token::LT.to_string(),
+                literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+            },
+            b'>' => token::Token {
+                token_type: token::GT.to_string(),
                 literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
             },
             b';' => token::Token {
@@ -77,12 +131,85 @@ impl Lexer {
                 token_type: token::EOF.to_string(),
                 literal: "".to_string(),
             },
-            _ => panic!("Unexpected character in lexer"),
+            _ => {
+                let character = self.ch as char;
+
+                if character.is_ascii_alphabetic() || character == '_' {
+                    let literal = self.read_identifier();
+                    let token_type = token::lookup_ident(literal.clone());
+                    // We need early return here because
+                    // in the end of function we run read_char(),
+                    // but read_identifier() already at last position
+                    return token::Token {
+                        token_type,
+                        literal,
+                    };
+                } else if character.is_digit(10) {
+                    // TODO: Add linenumber and position to track the error
+                    // and show output to user.
+                    let literal = self.read_number();
+                    let token_type = token::INT.to_string();
+                    // We need early return here because
+                    // in the end of function we run read_char(),
+                    // but read_number() already at last position
+                    return token::Token {
+                        token_type,
+                        literal,
+                    };
+                } else {
+                    token::Token {
+                        token_type: token::ILLEGAL.to_string(),
+                        literal: [self.ch].iter().map(|&c| c as char).collect::<String>(),
+                    }
+                }
+            }
         };
 
         self.read_char();
 
         tok
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while self.is_letter(self.ch) {
+            self.read_char()
+        }
+        self.input.as_bytes()[position as usize..self.position as usize]
+            .iter()
+            .map(|&c| c as char)
+            .collect::<String>()
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while (self.ch as char).is_digit(10) {
+            self.read_char()
+        }
+
+        self.input.as_bytes()[position as usize..self.position as usize]
+            .iter()
+            .map(|&c| c as char)
+            .collect::<String>()
+    }
+
+    fn peek_char(&self) -> u8 {
+        if self.read_position >= self.input.len() as u32 {
+            0
+        } else {
+            self.input.as_bytes()[self.read_position as usize]
+        }
+    }
+
+    fn is_letter(&self, c: u8) -> bool {
+        let character = c as char;
+        character.is_ascii_alphabetic() || character == '_'
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == b' ' || self.ch == b'\t' || self.ch == b'\n' || self.ch == b'\r' {
+            self.read_char()
+        }
     }
 }
 
@@ -92,7 +219,7 @@ mod tests {
     use crate::token;
 
     #[test]
-    fn test_next_token() {
+    fn test_next_token_basic() {
         let input = String::from("=+(){},;");
 
         let expected = vec![
@@ -105,6 +232,167 @@ mod tests {
             (token::COMMA.to_string(), String::from(",")),
             (token::SEMICOLON.to_string(), String::from(";")),
             (token::EOF.to_string(), String::from("")),
+        ];
+
+        let mut lexer = Lexer::new(input);
+
+        expected.into_iter().for_each(|(token_type, literal)| {
+            let token = lexer.next_token();
+
+            assert_eq!(token.token_type, token_type);
+            assert_eq!(token.literal, literal);
+        });
+    }
+
+    #[test]
+    fn test_next_token_clojurium_snippet() {
+        let input = r###"
+          let five = 5;
+          let ten = 10;
+
+          let add = fn(x, y) {
+            x + y;
+          };
+
+          let result = add(five, ten);
+        "###
+        .to_string();
+
+        let expected = vec![
+            (token::LET.to_string(), String::from("let")),
+            (token::IDENT.to_string(), String::from("five")),
+            (token::ASSIGN.to_string(), String::from("=")),
+            (token::INT.to_string(), String::from("5")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::LET.to_string(), String::from("let")),
+            (token::IDENT.to_string(), String::from("ten")),
+            (token::ASSIGN.to_string(), String::from("=")),
+            (token::INT.to_string(), String::from("10")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::LET.to_string(), String::from("let")),
+            (token::IDENT.to_string(), String::from("add")),
+            (token::ASSIGN.to_string(), String::from("=")),
+            (token::FUNCTION.to_string(), String::from("fn")),
+            (token::LPAREN.to_string(), String::from("(")),
+            (token::IDENT.to_string(), String::from("x")),
+            (token::COMMA.to_string(), String::from(",")),
+            (token::IDENT.to_string(), String::from("y")),
+            (token::RPAREN.to_string(), String::from(")")),
+            (token::LBRACE.to_string(), String::from("{")),
+            (token::IDENT.to_string(), String::from("x")),
+            (token::PLUS.to_string(), String::from("+")),
+            (token::IDENT.to_string(), String::from("y")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::RBRACE.to_string(), String::from("}")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::LET.to_string(), String::from("let")),
+            (token::IDENT.to_string(), String::from("result")),
+            (token::ASSIGN.to_string(), String::from("=")),
+            (token::IDENT.to_string(), String::from("add")),
+            (token::LPAREN.to_string(), String::from("(")),
+            (token::IDENT.to_string(), String::from("five")),
+            (token::COMMA.to_string(), String::from(",")),
+            (token::IDENT.to_string(), String::from("ten")),
+            (token::RPAREN.to_string(), String::from(")")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::EOF.to_string(), String::from("")),
+        ];
+
+        let mut lexer = Lexer::new(input);
+
+        expected.into_iter().for_each(|(token_type, literal)| {
+            let token = lexer.next_token();
+
+            assert_eq!(token.token_type, token_type);
+            assert_eq!(token.literal, literal);
+        });
+    }
+
+    #[test]
+    fn test_arithmetic_operators() {
+        let input = r###"
+         !-/*5;
+         5 < 10 > 5;
+       "###
+        .to_string();
+
+        let mut lexer = Lexer::new(input);
+
+        let expected = vec![
+            (token::BANG.to_string(), String::from("!")),
+            (token::MINUS.to_string(), String::from("-")),
+            (token::SLASH.to_string(), String::from("/")),
+            (token::ASTERISK.to_string(), String::from("*")),
+            (token::INT.to_string(), String::from("5")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::INT.to_string(), String::from("5")),
+            (token::LT.to_string(), String::from("<")),
+            (token::INT.to_string(), String::from("10")),
+            (token::GT.to_string(), String::from(">")),
+            (token::INT.to_string(), String::from("5")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+        ];
+
+        expected.into_iter().for_each(|(token_type, literal)| {
+            let token = lexer.next_token();
+
+            assert_eq!(token.token_type, token_type);
+            assert_eq!(token.literal, literal);
+        });
+    }
+
+    #[test]
+    fn test_flow_control_keywords() {
+        let input = r###"
+          if (2 < 3) {
+              return true;
+          } else {
+              return false;
+          }
+        "###
+        .to_string();
+
+        let expected = vec![
+            (token::IF.to_string(), String::from("if")),
+            (token::LPAREN.to_string(), String::from("(")),
+            (token::INT.to_string(), String::from("2")),
+            (token::LT.to_string(), String::from("<")),
+            (token::INT.to_string(), String::from("3")),
+            (token::RPAREN.to_string(), String::from(")")),
+            (token::LBRACE.to_string(), String::from("{")),
+            (token::RETURN.to_string(), String::from("return")),
+            (token::TRUE.to_string(), String::from("true")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::RBRACE.to_string(), String::from("}")),
+            (token::ELSE.to_string(), String::from("else")),
+            (token::LBRACE.to_string(), String::from("{")),
+            (token::RETURN.to_string(), String::from("return")),
+            (token::FALSE.to_string(), String::from("false")),
+            (token::SEMICOLON.to_string(), String::from(";")),
+            (token::RBRACE.to_string(), String::from("}")),
+        ];
+
+        let mut lexer = Lexer::new(input);
+
+        expected.into_iter().for_each(|(token_type, literal)| {
+            let token = lexer.next_token();
+
+            assert_eq!(token.token_type, token_type);
+            assert_eq!(token.literal, literal);
+        });
+    }
+
+    #[test]
+    fn test_multichar_operators() {
+        let input = r###"1 == 1 1 != 2"###.to_string();
+
+        let expected = vec![
+            (token::INT.to_string(), String::from("1")),
+            (token::EQ.to_string(), String::from("==")),
+            (token::INT.to_string(), String::from("1")),
+            (token::INT.to_string(), String::from("1")),
+            (token::NOT_EQ.to_string(), String::from("!=")),
+            (token::INT.to_string(), String::from("2")),
         ];
 
         let mut lexer = Lexer::new(input);
--- src/token.rs
+++ src/token.rs
@@ -1,9 +1,31 @@
+use std::collections::HashMap;
+
 type TokenType = String;
 
 #[derive(Debug, Clone)]
 pub struct Token {
     pub token_type: TokenType,
     pub literal: String,
+}
+
+pub fn lookup_ident(ident: String) -> TokenType {
+    let keywords: HashMap<String, TokenType> = [
+        ("fn".to_string(), FUNCTION.to_string()),
+        ("let".to_string(), LET.to_string()),
+        ("true".to_string(), TRUE.to_string()),
+        ("false".to_string(),  FALSE.to_string()),
+        ("if".to_string(),     IF.to_string()),
+        ("else".to_string(),   ELSE.to_string()),
+        ("return".to_string(), RETURN.to_string()),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    match keywords.get(&ident) {
+        Some(keyword) => keyword.clone(),
+        None => IDENT.to_string(),
+    }
 }
 
 pub const ILLEGAL: &str = "ILLEGAL";
@@ -16,6 +38,16 @@ pub const INT: &str = "INT";
 // Operators
 pub const ASSIGN: &str = "=";
 pub const PLUS: &str = "+";
+pub const MINUS: &str = "-";
+pub const BANG: &str = "!";
+pub const ASTERISK: &str = "*";
+pub const SLASH: &str = "/";
+
+pub const LT: &str = "<";
+pub const GT: &str = ">";
+
+pub const EQ: &str = "==";
+pub const NOT_EQ: &str = "!=";
 
 // Delimiters
 pub const COMMA: &str = ",";
@@ -29,3 +61,8 @@ pub const RBRACE: &str = "}";
 // Keywords
 pub const FUNCTION: &str = "FUNCTION";
 pub const LET: &str = "LET";
+pub const TRUE: &str = "TRUE";
+pub const FALSE: &str = "FALSE";
+pub const IF: &str = "IF";
+pub const ELSE: &str = "ELSE";
+pub const RETURN: &str = "RETURN";
