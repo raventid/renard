8b559c43594b06a5825973b8626293a0b9ab45c1
Julian Pokrovsky <juliankul@gmail.com>
2019-03-07 18:25:55 +0300

    fixes bug in parser peek_error function

--- src/parser.rs
+++ src/parser.rs
@@ -69,6 +69,7 @@ impl Parser {
         if self.peek_token.token_type == token::IDENT {
             self.next_token();
         } else {
+            self.peek_error(token::IDENT.to_string());
             return None;
         }
 
@@ -80,6 +81,7 @@ impl Parser {
         if self.peek_token.token_type == token::ASSIGN {
             self.next_token();
         } else {
+            self.peek_error(token::ASSIGN.to_string());
             return None;
         }
 
@@ -133,7 +135,54 @@ mod tests {
             panic!("A few parsing error encountered, see them above.");
         }
 
-        dbg!(&program);
+        assert_eq!(program.statements.len(), 3);
+
+        let expected = vec!["x".to_string(), "y".to_string(), "bebe".to_string()];
+
+        program
+            .statements
+            .into_iter()
+            .zip(expected.into_iter())
+            .for_each(|(statement, expected_identifier)| {
+                assert_eq!(statement.token_literal(), "let");
+
+                let let_statement = match statement {
+                    Statements::LetStatement(statement) => statement,
+                    _ => panic!("I didn't expected anything besides `let` statement"),
+                };
+
+                assert_eq!(let_statement.name.value, expected_identifier);
+
+                assert_eq!(let_statement.name.token_literal(), expected_identifier);
+            });
+    }
+
+    #[test]
+    fn test_broken_let_statements_and_check_if_errors_appear() {
+        let input = r###"
+          let x = 5;
+          let y = 10;
+          let bebe 101010;
+        "###
+        .to_string();
+
+        let lexer = lexer::Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = match parser.parse_program() {
+            Some(program) => program,
+            None => panic!("Could not parse program"),
+        };
+
+        // We would like to accumulate every error in program
+        // and later render them to user.
+        if !parser.errors.is_empty() {
+            println!("Parser encountered {} errors", parser.errors.len());
+            for error in parser.errors {
+                println!("parser error: {}", error);
+            }
+            panic!("A few parsing error encountered, see them above.");
+        }
 
         assert_eq!(program.statements.len(), 3);
 
