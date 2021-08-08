dbee45be7d6938f4fa5cfa2cf5a5ced2e4f98ea0
Julian Pokrovsky <juliankul@gmail.com>
2021-08-08 23:45:33 +0300

    [repl] remove redundant line

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,5 @@ fn main() {
     // how to get current user name in crossplatform style?
     let _username = "user".to_string();
     println!("Dear user, welcome to Renard REPL!");
-    println!("Some more advanced description of what's going on here");
     repl::start();
 }
